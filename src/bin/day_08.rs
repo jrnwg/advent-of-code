2fd4a0daea8b230ae95f7069725505e5df0dd5d1
jrnwg <189479406+jrnwg@users.noreply.github.com>
2025-12-08 22:03:01 +0100

    day 8 refactor and optimization

--- src/bin/day_08.rs
+++ src/bin/day_08.rs
@@ -2,136 +2,126 @@ use advent_of_code::get_input;
 
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 const MAX_PAIRS: usize = 1000;
 
 fn main() {
     let input = get_input(8);
-    println!("Part One: {}", part_one(&input));
-    println!("Part Two: {}", part_two(&input));
+    println!("Part One: {}", part_one(&input, MAX_PAIRS));
+    println!("Part Two: {}", part_two(&input, MAX_PAIRS));
 }
 
-fn part_one(input: &str) -> usize {
+type Coord = (usize, usize, usize);
+
+fn init_heap(input: &str) -> BinaryHeap<Reverse<(i64, (Coord, Coord))>> {
     let coords = input
         .lines()
         .map(|line| {
-            let mut parts = line.split(',').map(|s| s.parse::<i64>().unwrap());
+            let mut parts = line.split(',').map(|s| s.parse::<usize>().unwrap());
             (
                 parts.next().unwrap(),
                 parts.next().unwrap(),
                 parts.next().unwrap(),
             )
         })
-        .collect::<Vec<(i64, i64, i64)>>();
+        .collect::<Vec<Coord>>();
 
     let mut heap = BinaryHeap::new();
     for (idx, (x, y, z)) in coords.iter().enumerate() {
         for (nx, ny, nz) in &coords[idx + 1..] {
-            let d = (x - nx).pow(2) + (y - ny).pow(2) + (z - nz).pow(2);
+            let d: i64 = (*x as i64 - *nx as i64).pow(2)
+                + (*y as i64 - *ny as i64).pow(2)
+                + (*z as i64 - *nz as i64).pow(2);
             heap.push(Reverse((d, ((*x, *y, *z), (*nx, *ny, *nz)))));
         }
     }
+    heap
+}
 
-    let mut circuits: Vec<HashSet<(i64, i64, i64)>> = Vec::new();
+fn merge_circuits(
+    idx_cache: &mut HashMap<Coord, usize>,
+    circuits: &mut Vec<HashSet<Coord>>,
+    p1: Coord,
+    p2: Coord,
+) -> usize {
+    let mut found_idxs: HashSet<usize> = HashSet::new();
+
+    if let Some(&idx) = idx_cache.get(&p1) {
+        found_idxs.insert(idx);
+    }
+    if let Some(&idx) = idx_cache.get(&p2) {
+        found_idxs.insert(idx);
+    }
+
+    let idx = match found_idxs.len() {
+        0 => {
+            let new_circuit = HashSet::new();
+            circuits.push(new_circuit);
+            circuits.len() - 1
+        }
+        1 => *found_idxs.iter().next().unwrap(),
+        2 => {
+            let idx1 = *found_idxs.iter().next().unwrap();
+            let idx2 = *found_idxs.iter().nth(1).unwrap();
+
+            let to_merge = std::mem::take(&mut circuits[idx2]);
+            for coord in to_merge {
+                idx_cache.insert(coord, idx1);
+                circuits[idx1].insert(coord);
+            }
+
+            idx1
+        }
+        _ => panic!("More than two circuits found for points"),
+    };
+
+    circuits[idx].insert(p1);
+    circuits[idx].insert(p2);
+
+    idx_cache.insert(p1, idx);
+    idx_cache.insert(p2, idx);
+    idx
+}
+
+fn part_one(input: &str, max_pairs: usize) -> usize {
+    let mut heap = init_heap(input);
+
+    let mut idx_cache: HashMap<Coord, usize> = HashMap::new();
+    let mut circuits: Vec<HashSet<Coord>> = Vec::new();
     let mut count = 0;
-    while !heap.is_empty() && count < MAX_PAIRS {
+    while !heap.is_empty() && count < max_pairs {
         count += 1;
 
         let Reverse((_, (p1, p2))) = heap.pop().unwrap();
-        let mut found_idxs: Vec<usize> = Vec::new();
-        for (idx, circuit) in circuits.iter().enumerate() {
-            if circuit.contains(&p1) || circuit.contains(&p2) {
-                found_idxs.push(idx);
-            }
-        }
-
-        match found_idxs.len() {
-            0 => {
-                let mut new_circuit = HashSet::new();
-                new_circuit.insert(p1);
-                new_circuit.insert(p2);
-                circuits.push(new_circuit);
-            }
-            1 => {
-                let circuit = &mut circuits[found_idxs[0]];
-                circuit.insert(p1);
-                circuit.insert(p2);
-            }
-            _ => {
-                let to_merge = circuits.swap_remove(found_idxs[1]);
-                circuits[found_idxs[0]].extend(to_merge);
-                circuits[found_idxs[0]].insert(p1);
-                circuits[found_idxs[0]].insert(p2);
-            }
-        }
+        merge_circuits(&mut idx_cache, &mut circuits, p1, p2);
     }
 
-    let mut sizes: Vec<usize> = circuits.iter().map(|c| c.len()).collect();
-    sizes.sort_by_key(|&len| std::cmp::Reverse(len));
-    sizes.iter().take(3).product::<usize>()
+    let mut sizes: Vec<usize> = circuits
+        .iter()
+        .map(|c| c.len())
+        .filter(|&len| len > 0)
+        .collect();
+    sizes.sort_unstable_by(|a, b| b.cmp(a));
+    sizes.iter().take(3).product()
 }
 
-fn part_two(input: &str) -> usize {
-    let coords = input
-        .lines()
-        .map(|line| {
-            let mut parts = line.split(',').map(|s| s.parse::<i64>().unwrap());
-            (
-                parts.next().unwrap(),
-                parts.next().unwrap(),
-                parts.next().unwrap(),
-            )
-        })
-        .collect::<Vec<(i64, i64, i64)>>();
+fn part_two(input: &str, max_pairs: usize) -> usize {
+    let mut heap = init_heap(input);
 
-    let mut heap = BinaryHeap::new();
-    for (idx, (x, y, z)) in coords.iter().enumerate() {
-        for (nx, ny, nz) in &coords[idx + 1..] {
-            let d = (x - nx).pow(2) + (y - ny).pow(2) + (z - nz).pow(2);
-            heap.push(Reverse((d, ((*x, *y, *z), (*nx, *ny, *nz)))));
-        }
-    }
-
-    let mut circuits: Vec<HashSet<(i64, i64, i64)>> = Vec::new();
+    let mut idx_cache: HashMap<Coord, usize> = HashMap::new();
+    let mut circuits: Vec<HashSet<Coord>> = Vec::new();
     while !heap.is_empty() {
         let Reverse((_, (p1, p2))) = heap.pop().unwrap();
-        let mut found_idxs: Vec<usize> = Vec::new();
-        for (idx, circuit) in circuits.iter().enumerate() {
-            if circuit.contains(&p1) || circuit.contains(&p2) {
-                found_idxs.push(idx);
-            }
-        }
+        let idx = merge_circuits(&mut idx_cache, &mut circuits, p1, p2);
 
-        match found_idxs.len() {
-            0 => {
-                let mut new_circuit = HashSet::new();
-                new_circuit.insert(p1);
-                new_circuit.insert(p2);
-                circuits.push(new_circuit);
-            }
-            1 => {
-                let circuit = &mut circuits[found_idxs[0]];
-                circuit.insert(p1);
-                circuit.insert(p2);
-            }
-            _ => {
-                let to_merge = circuits.swap_remove(found_idxs[1]);
-                circuits[found_idxs[0]].extend(to_merge);
-                circuits[found_idxs[0]].insert(p1);
-                circuits[found_idxs[0]].insert(p2);
-            }
-        }
-
-        let mut sizes: Vec<usize> = circuits.iter().map(|c| c.len()).collect();
-        sizes.sort_by_key(|&len| std::cmp::Reverse(len));
-
-        if *sizes.first().unwrap() == 1000 {
-            return p1.0 as usize * p2.0 as usize;
+        if circuits[idx].len() >= max_pairs {
+            return p1.0 * p2.0;
         }
     }
 
-    0
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -161,11 +151,11 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one(TEST_INPUT), 40);
+        assert_eq!(part_one(TEST_INPUT, 10), 40);
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_two(TEST_INPUT), 25272);
+        assert_eq!(part_two(TEST_INPUT, 20), 25272);
     }
 }
