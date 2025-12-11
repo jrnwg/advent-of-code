2f3388b9ee36313e510954eaa9665d3ee811d6c0
jrnwg <189479406+jrnwg@users.noreply.github.com>
2025-12-11 13:32:13 +0100

    day 11 part two

--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use advent_of_code::{get_input, solve};
+use cached::proc_macro::cached;
 
 fn main() {
     let input = get_input(11);
@@ -20,7 +21,7 @@ fn init_factory(input: &str) -> HashMap<&str, Vec<&str>> {
     factory
 }
 
-fn dfs(factory: &HashMap<&str, Vec<&str>>, device: &str) -> usize {
+fn dfs_one(factory: &HashMap<&str, Vec<&str>>, device: &str) -> usize {
     if device == "out" {
         return 1;
     }
@@ -29,24 +30,65 @@ fn dfs(factory: &HashMap<&str, Vec<&str>>, device: &str) -> usize {
         .get(device)
         .unwrap()
         .iter()
-        .map(|d| dfs(factory, d))
+        .map(|d| dfs_one(factory, d))
         .sum()
 }
 
 fn part_one(input: &str) -> usize {
     let factory = init_factory(input);
-    dfs(&factory, "you")
+    dfs_one(&factory, "you")
 }
 
-fn part_two(_input: &str) -> usize {
-    unimplemented!()
+#[cached(
+    size = 1000,
+    key = "(String, Vec<(String, bool)>)",
+    convert = r#"{
+        let mut seen_vec: Vec<(String, bool)> = seen.iter()
+            .map(|(k, v)| (k.clone(), *v))
+            .collect();
+        seen_vec.sort();
+        (device.to_string(), seen_vec)
+    }"#
+)]
+fn dfs_two(
+    factory: &HashMap<&str, Vec<&str>>,
+    device: &str,
+    seen: &mut HashMap<String, bool>,
+) -> usize {
+    if device == "out" {
+        if seen.values().all(|&v| v) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if seen.contains_key(device) {
+        seen.insert(device.to_string(), true);
+    }
+
+    factory
+        .get(device)
+        .unwrap()
+        .iter()
+        .map(|d| dfs_two(factory, d, &mut seen.clone()))
+        .sum()
+}
+
+fn part_two(input: &str) -> usize {
+    let factory = init_factory(input);
+
+    let mut seen: HashMap<String, bool> = HashMap::new();
+    seen.insert("dac".to_string(), false);
+    seen.insert("fft".to_string(), false);
+    dfs_two(&factory, "svr", &mut seen)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "aaa: you hhh
+    const TEST_INPUT_1: &str = "aaa: you hhh
 you: bbb ccc
 bbb: ddd eee
 ccc: ddd eee fff
@@ -57,13 +99,27 @@ ggg: out
 hhh: ccc fff iii
 iii: out";
 
+    const TEST_INPUT_2: &str = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
+
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one(TEST_INPUT), 5);
+        assert_eq!(part_one(TEST_INPUT_1), 5);
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_two(TEST_INPUT), 0);
+        assert_eq!(part_two(TEST_INPUT_2), 2);
     }
 }
